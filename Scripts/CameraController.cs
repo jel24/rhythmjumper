ff652cf6117fa234919aadf7025f63ff2ca2da9e
DOMINGO\Jake <jake@jakelandry.com>
2017-01-14 13:04:06 -0800

    commit 1/14 did some cool camera stuff

---
+++ Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	public GameObject background;
+	public float thresholdX;
+	public float thresholdY;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		float transformX = 0f;
+		float transformY = 0f;
+		Vector3 playerPos = player.transform.position;
+		Vector3 camPos = transform.position;
+
+		if (Mathf.Abs (playerPos.x - camPos.x) > thresholdX) {
+			transformX = (playerPos.x - camPos.x) * Time.deltaTime;
+		}
+
+		if (Mathf.Abs (playerPos.y - camPos.y) > thresholdY) {
+			transformY = (playerPos.y - camPos.y) * Time.deltaTime * 2;
+		} 
+
+		if (transformX != 0f || transformY != 0f) {
+			Vector3 translationVector = new Vector3(transformX, transformY, 0f);
+			transform.Translate(translationVector);
+			background.transform.Translate(translationVector/5f);
+		}
+
+
+	}
+}
