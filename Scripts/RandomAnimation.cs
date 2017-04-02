0b85b79bb0150dbb84ac9e2d0e0e0d070a799bf8
Jake Landry <jlandry@luxoft.com>
2017-04-02 14:37:14 -0700

    commit 4/1 added grass to level 2

---
+++ Scripts/RandomAnimation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomAnimation : MonoBehaviour {
+
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+		Invoke("StartAnimating", Random.Range(0f, 3f));
+	}
+
+	private void StartAnimating(){
+		animator.SetTrigger ("start");
+	}
+}
