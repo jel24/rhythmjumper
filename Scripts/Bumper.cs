7fd37547c870282250b9e216a71e35864ea31150
Jake Landry <jlandry@luxoft.com>
2017-04-01 23:40:43 -0700

    commit 4/1 added bumper; adjusted level 2 mechanics

---
+++ Scripts/Bumper.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bumper : MonoBehaviour {
+
+	public Vector2 force;
+
+	void OnTriggerStay2D(Collider2D coll){
+		if (coll.gameObject.tag == "Player") {
+			coll.GetComponentInParent<Rigidbody2D>().AddForce (force);
+		}
+	}
+}
