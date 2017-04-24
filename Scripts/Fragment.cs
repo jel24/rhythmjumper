bd0b065272fd28e32885d5dadd419133b7d7c079
Jake Landry <jlandry@luxoft.com>
2017-04-23 19:04:32 -0700

    commit 4/23 added fragment graphics and particle emitters

---
+++ Scripts/Fragment.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fragment : MonoBehaviour {
+
+
+	void OnTriggerEnter2D(Collider2D coll)
+	{
+		if (coll.gameObject.tag == "Player") {
+			Destroy (this.gameObject);
+			print ("Collided!");
+		}
+	}
+}
--- Scripts/Spikes.cs
+++ Scripts/Spikes.cs
@@ -3,11 +3,6 @@ using System.Collections;
 
 public class Spikes : MonoBehaviour {
 
-	void Start(){			
-		if (gameObject.name.Contains("Cloud")) {
-			this.transform.Rotate(new Vector3(0f, 0f, (Random.Range (0f, 360f))));
-		}
-	}
 
 	void OnTriggerEnter2D (Collider2D coll)
 	{
