c69a7efedb227d1c710735353d387b1f721ba944
Jake Landry <jake@jakelandry.com>
2017-12-26 16:30:03 -0800

    commit 12/26 added fade effect to tempblocks, changed behavior to change boxcollider to trigger instead of disabling gameobject

--- Scripts/TempBlock.cs
+++ Scripts/TempBlock.cs
@@ -8,17 +8,27 @@ public class TempBlock : MonoBehaviour {
 	public int offset;
 
 	private int ticker;
+	private BoxCollider2D hitbox;
+	private Animator anim;
 
 	// Use this for initialization
 	void Start () {
 		ticker = 0 - offset;
+		hitbox = GetComponent<BoxCollider2D> ();
+		anim = GetComponent<Animator> ();
 		FindObjectOfType<PlayerController> ().AddFunctionToJump (Jump);
 	}
 	
 	void Jump(){
 		ticker++;
 		if (ticker == jumpsRequired) {
-			gameObject.SetActive (!gameObject.active);
+			//gameObject.SetActive (!gameObject.active);
+			hitbox.isTrigger = !hitbox.isTrigger;
+			if (hitbox.isTrigger) {
+				anim.SetBool ("Hidden", true);
+			} else {
+				anim.SetBool ("Hidden", false);
+			}
 			ticker = 0;
 		}
 	}
