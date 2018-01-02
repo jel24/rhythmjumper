4eb9f4159f6c56c1d7e0773217612214c4c06002
Jake Landry <jlandry@luxoft.com>
2018-01-01 23:11:49 -0800

    commit 1/1 updated mazebutton to press, fixed wall cling sprite, adjusted textures

--- Scripts/MazeButton.cs
+++ Scripts/MazeButton.cs
@@ -6,14 +6,48 @@ public class MazeButton : MonoBehaviour {
 
 	public MazeDirection direction;
 	public Maze maze;
+	private bool compressed;
+	private Vector3 loweredPosition;
+	private Vector3 originalPosition;
 
-	void OnCollisionStay2D(Collision2D coll)
+
+
+	void Start(){
+		compressed = false;
+		loweredPosition = new Vector3 (transform.position.x, transform.position.y - .1f, transform.position.z);
+		originalPosition = transform.position;
+
+	}
+
+	void OnTriggerStay2D(Collider2D coll)
 	{
 		
 
-		if (coll.collider.gameObject.tag == "Player") {
+		if (coll.gameObject.tag == "Player") {
 			//Debug.Log ("Adding.");
 			maze.SetDirection (direction);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		if (coll.gameObject.tag == "Player") {
+			print ("Enter");
+			if (!compressed) {
+				compressed = true;
+				transform.position = loweredPosition;
+			}
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D coll) {
+
+		if (coll.gameObject.tag == "Player") {
+			print ("Exit");
+
+			if (compressed) {
+				compressed = false;
+				transform.position = originalPosition;
+			}
 		}
 	}
 }
